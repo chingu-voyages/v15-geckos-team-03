d24a7ba04dc54200ee5f27515cf07f4c806781f4
shashidar mandalozu <sasitech18@gmail.com>
2020-01-19 02:44:19 +0000

    Added navigation features

---
+++ index.js
@@ -0,0 +1,9 @@
+var navList = document.getElementById("navLists");
+function show() {
+    navList.classList.add("_Menus-show");
+}
+
+function hide() {
+    navList.classList.remove("_Menus-show");
+}
+
