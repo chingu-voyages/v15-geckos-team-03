972cad7adc3f1dfa1f00f0e570225b8eca63cfb0
shashidar mandalozu <sasitech18@gmail.com>
2020-02-12 01:12:16 +0000

    Merged navBar and mainSection with Gif API functionality

--- index.js
+++ index.js
@@ -10,3 +10,35 @@ $(document).ready(function(){
         }
     );
 });
+
+
+// Giphy API Start
+
+const giphy_api_key = "xOsO9SpwmmPd30XSkhzM7x9nB5GLIXva";
+
+//Connect API key and Search Endpoint url
+const getGifs = () => {
+  const gif = document.getElementById("gifs").value || "cats";
+
+  const search_endpoint = `https://api.giphy.com/v1/gifs/search?q=${gif}&api_key=${giphy_api_key}`;
+
+  //Fetching Data from API
+  fetch(search_endpoint)
+    .then(api_res => api_res.json())
+    .then(api_output => {
+      const gif_array = api_output.data.map(gif => gif.images.fixed_width.url);
+      console.log(gif_array);
+
+      const gifs = gif_array.map( gif =>
+            `<div class="flex-fill">
+			 <img src=${gif}>
+			 </div>`
+        ).join();
+      document.getElementById("display_gif").innerHTML = gifs;
+    })
+    .catch(err => console.log(err));
+};
+//Ending
+window.addEventListener("load", getGifs);
+document.getElementById("search_gif").addEventListener("click", getGifs);
+// Giphy API End
